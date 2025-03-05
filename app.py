80fbb9f812077d8304f3850870d007b13f92154d
Kirankumar Ashok Patil <155320660+kirankumarashokpatil@users.noreply.github.com>
2025-03-05 13:40:58 +0000

    Update app.py
    Added Port = 8050

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ register_callbacks(app)
 # RUN SERVER
 ###############################################################################
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
