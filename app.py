8caa9c8316bf0263e145fd4a99021f926fd8d68c
Kirankumar Ashok Patil <155320660+kirankumarashokpatil@users.noreply.github.com>
2025-03-05 16:08:34 +0000

    app.py changes

--- app.py
+++ app.py
@@ -234,5 +234,8 @@ register_callbacks(app)
 ###############################################################################
 # RUN SERVER
 ###############################################################################
+# Important: Export the underlying Flask server
+server = app.server
+
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", port=8050, debug=True)
